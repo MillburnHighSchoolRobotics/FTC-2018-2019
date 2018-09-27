2a6776926f65d1390e5a601ee95571e61b1218fa
David Shustin <david.shustin@gmail.com>
2018-09-27 11:34:44 -0400

    Complete preliminary TeleOp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CompetitionUpdate/RoverRuckusTeleOpUpdate.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CompetitionUpdate/RoverRuckusTeleOpUpdate.java
@@ -1,9 +1,12 @@
 package org.firstinspires.ftc.teamcode.CompetitionUpdate;
 
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
 import org.firstinspires.ftc.teamcode.ReflectionUpdateThread;
 
 import virtualRobot.logicThreads.competition.RoverRuckusTeleOpLogic;
 
+@TeleOp(name = "Rover Ruckus TeleOp", group = "competition")
 public class RoverRuckusTeleOpUpdate extends ReflectionUpdateThread {
     @Override
     public void setLogicThread() {
--- TeamCode/src/main/java/virtualRobot/logicThreads/competition/RoverRuckusTeleOpLogic.java
+++ TeamCode/src/main/java/virtualRobot/logicThreads/competition/RoverRuckusTeleOpLogic.java
@@ -1,10 +1,37 @@
 package virtualRobot.logicThreads.competition;
 
+import virtualRobot.JoystickController;
 import virtualRobot.LogicThread;
+import virtualRobot.utils.MathUtils;
 
 public class RoverRuckusTeleOpLogic extends LogicThread {
     @Override
     protected void realRun() throws InterruptedException {
+        double L = 0, R = 0;
+        double scale = 0.7;
+        JoystickController controller1;
+        JoystickController controller2;
+        controller1 = robot.getJoystickController1();
+        controller2 = robot.getJoystickController2();
 
+
+        while (true) {
+            if (!MathUtils.equals(controller1.getValue(JoystickController.Y_1), 0, 0.005)) {
+                double power = controller1.getValue(JoystickController.Y_1);
+                L = power;
+                R = power;
+            } else if (!MathUtils.equals(controller1.getValue(JoystickController.X_2), 0, 0.005)) {
+                double power = controller1.getValue(JoystickController.X_2);
+                L = power;
+                R = -power;
+            }
+            robot.getLBMotor().setPower(scale * L);
+            robot.getLFMotor().setPower(scale * L);
+            robot.getRBMotor().setPower(scale * R);
+            robot.getRFMotor().setPower(scale * R);
+            if (Thread.currentThread().isInterrupted())
+                throw new InterruptedException();
+            Thread.sleep(10);
+        }
     }
 }
