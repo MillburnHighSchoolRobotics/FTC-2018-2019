71340894cf313f0b1dd97d20c5ab9421272598c8
David Shustin <david.shustin@gmail.com>
2018-10-26 21:05:16 -0400

    Add auton????

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NonUpdateCompetitionOpModes/BlueAuton1.java
@@ -0,0 +1,101 @@
+package org.firstinspires.ftc.teamcode.NonUpdateCompetitionOpModes;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class BlueAuton1 extends LinearOpMode {
+    DcMotor lf = hardwareMap.dcMotor.get("lf");
+    DcMotor lb = hardwareMap.dcMotor.get("lb");
+    DcMotor rf = hardwareMap.dcMotor.get("rf");
+    DcMotor rb = hardwareMap.dcMotor.get("rb");
+    Servo marker = hardwareMap.servo.get("marker");
+
+    final float botWidth = 16.5f; //inches
+    final float botRadius = botWidth/2; //in
+    final float wheelWidth = 3; //in
+    final float wheelRadius = wheelWidth/2; //in
+    final int ticksPerRev = 680; //ticks per rev
+//    final float targetRadius = 10+c; //inches
+//    final double targetSpeed =4; //inches/sec
+//    final double encoderSpeed = (targetSpeed/(2*Math.PI*wheelRadius)) * ticksPerRev; //encoders per sec
+
+    public int distToEncoder(double dist) { //inches
+        return (int)(dist/(2*Math.PI*wheelRadius) * ticksPerRev);
+    }
+
+    public int rotateToEncoder(double rad) {
+        double dist = botRadius*rad;
+        return distToEncoder(dist);
+    }
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        lf.setDirection(DcMotorSimple.Direction.FORWARD);
+        lb.setDirection(DcMotorSimple.Direction.FORWARD);
+        rf.setDirection(DcMotorSimple.Direction.REVERSE);
+        rb.setDirection(DcMotorSimple.Direction.REVERSE);
+//        for (int x = 0; x < 4; x++) {
+            initializeMotor(new DcMotor[]{lf, lb, rf, rb});
+//        }
+
+        //sampling
+        translate(0.7,distToEncoder(15));
+        int num = 2; //which mineral is the gold mineral one
+        switch (num) { //TODO: actually put values
+            case 1:
+                rotate(-0.5,-1);
+                translate(0.7,1);
+                rotate(0.5, 1);
+                translate(0.7,1);
+                break;
+            case 2:
+                translate(0.7,distToEncoder(48));
+                break;
+            case 3:
+                rotate(0.5,1);
+                translate(0.7,1);
+                rotate(-0.5, -1);
+                translate(0.7,1);
+                break;
+        }
+
+
+        //place marker
+        marker.setPosition(0);
+        rotate(0.5,rotateToEncoder(Math.toRadians(45)));
+        translate(0.7,distToEncoder(115));
+    }
+    public void initializeMotor(DcMotor[] motors) {
+        for (DcMotor motor : motors) {
+            motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+//            motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+//            motor.setTargetPosition(0);
+        }
+    }
+
+    public void moveToPosition(DcMotor[] motors, double[] power, int[] position) {
+        for (int x = 0; x < motors.length; x++) {
+            motors[x].setPower(power[x]);
+            motors[x].setTargetPosition(position[x]);
+            motors[x].setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        }
+        while (motors[0].isBusy() || motors[1].isBusy() || motors[2].isBusy() || motors[3].isBusy()) {
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException ex) {}
+        }
+        for (DcMotor motor : motors) {
+            motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        }
+    }
+
+    public void translate(double power, int positionChange) {
+        moveToPosition(new DcMotor[] {lf,lb,rf,rb}, new double[] {power,power,power,power}, new int[] {(lf.getCurrentPosition()+positionChange),(lb.getCurrentPosition()+positionChange),(rf.getCurrentPosition()+positionChange),(rb.getCurrentPosition()+positionChange)});
+    }
+
+    public void rotate(double power, int positionChange) {
+        moveToPosition(new DcMotor[] {lf,lb,rf,rb}, new double[] {power,power,-power,-power},  new int[] {(lf.getCurrentPosition()+positionChange),(lb.getCurrentPosition()+positionChange),(rf.getCurrentPosition()-positionChange),(rb.getCurrentPosition()-positionChange)});
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NonUpdateCompetitionOpModes/BlueAuton2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NonUpdateCompetitionOpModes/BlueAuton2.java
@@ -3,12 +3,29 @@ package org.firstinspires.ftc.teamcode.NonUpdateCompetitionOpModes;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 
-public class BlueAuton extends LinearOpMode {
+public class BlueAuton2 extends LinearOpMode {
     DcMotor lf = hardwareMap.dcMotor.get("lf");
     DcMotor lb = hardwareMap.dcMotor.get("lb");
     DcMotor rf = hardwareMap.dcMotor.get("rf");
     DcMotor rb = hardwareMap.dcMotor.get("rb");
+    Servo marker = hardwareMap.servo.get("marker");
+    final float botWidth = 16.5f; //inches
+    final float botRadius = botWidth/2; //in
+    final float wheelWidth = 3; //in
+    final float wheelRadius = wheelWidth/2; //in
+    final int ticksPerRev = 680; //ticks per rev
+
+
+    public int distToEncoder(double dist) { //inches
+        return (int)(dist/(2*Math.PI*wheelRadius) * ticksPerRev);
+    }
+
+    public int rotateToEncoder(double rad) {
+        double dist = botRadius*rad;
+        return distToEncoder(dist);
+    }
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -16,17 +33,18 @@ public class BlueAuton extends LinearOpMode {
         lb.setDirection(DcMotorSimple.Direction.FORWARD);
         rf.setDirection(DcMotorSimple.Direction.REVERSE);
         rb.setDirection(DcMotorSimple.Direction.REVERSE);
-        for (int x = 0; x < 4; x++) {
+//        for (int x = 0; x < 4; x++) {
             initializeMotor(new DcMotor[]{lf, lb, rf, rb});
-        }
+//        }
 
         //sampling
-        translate(0.7,1);
-        int num = 1; //which mineral is the gold mineral one
+        translate(0.7,distToEncoder(18));
+        int num = 0;//TODO:Fix //which mineral is the gold mineral one
         switch (num) {
             case 1:
                 rotate(-0.5,-1);
                 translate(0.7,1);
+                rotate(0.5, 1);
                 break;
             case 2:
                 translate(0.7,1);
@@ -34,34 +52,24 @@ public class BlueAuton extends LinearOpMode {
             case 3:
                 rotate(0.5,1);
                 translate(0.7,1);
+                rotate(-0.5, -1);
+                break;
+            default:
                 break;
         }
-
-
-        //claiming
-        int position = 1; //which blue
-        switch (position) {
-            case 1:
-                rotate(-0.5,-1);
-                translate(0.7,1);
-                break;
-            case 2:
-                translate(0.9,1);
-                break;
-        }
-        //place marker
-        rotate(-0.5,-1);
-        translate(0.7,1);
-
-        //park
-        rotate(0.5,1);
-        translate(0.9,1);
+        rotate(-0.5, rotateToEncoder(Math.toRadians(-90)));//TODO:Add global variable for speed
+        translate(0.7, distToEncoder(48));//TODO:See above immortal TODO
+        rotate(0.5, rotateToEncoder(-45));
+        translate(0.7, distToEncoder(39));
+        marker.setPosition(0);
+//        rotate(-0.5,-1);
+        translate(-0.7,distToEncoder(-110));
     }
     public void initializeMotor(DcMotor[] motors) {
         for (DcMotor motor : motors) {
-            motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-            motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            motor.setTargetPosition(0);
+            motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+//            motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+//            motor.setTargetPosition(0);
         }
     }
 
@@ -69,11 +77,15 @@ public class BlueAuton extends LinearOpMode {
         for (int x = 0; x < motors.length; x++) {
             motors[x].setPower(power[x]);
             motors[x].setTargetPosition(position[x]);
+            motors[x].setMode(DcMotor.RunMode.RUN_TO_POSITION);
         }
         while (motors[0].isBusy() || motors[1].isBusy() || motors[2].isBusy() || motors[3].isBusy()) {
             try {
                 Thread.sleep(10);
             } catch (InterruptedException ex) {}
+        }
+        for (DcMotor motor : motors) {
+            motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         }
     }
 
