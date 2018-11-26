1025bb65d3f68008049891fd08548beee23128b6
SahilShah518 <sahilshah379@gmail.com>
2018-11-26 16:17:28 -0500

    rip needa commit this

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestingOpModes/ZeroPowerMotorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestingOpModes/ZeroPowerMotorTest.java
@@ -4,15 +4,19 @@ package org.firstinspires.ftc.teamcode.TestingOpModes;
 import android.util.Log;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 
 @Autonomous(name = "Zero Power Motor Test", group = "Testing")
-public class ZeroPowerMotorTest extends LinearOpMode {
+public class ZeroPowerMotorTest extends OpMode {
     @Override
-    public void runOpMode() throws InterruptedException {
+    public void init() {
         DcMotorEx motor = (DcMotorEx) hardwareMap.dcMotor.get("motor1");
         motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    }
+
+    @Override
+    public void loop() {
     }
 }
