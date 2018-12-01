2a7e220c012b75dc456c1d93cd4e1a469f130bb2
SahilShah518 <sahilshah379@gmail.com>
2018-11-30 22:08:29 -0500

    cv in blueauton1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NonUpdateCompetitionOpModes/BlueAuton1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NonUpdateCompetitionOpModes/BlueAuton1.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode.NonUpdateCompetitionOpModes;
 
+import android.util.Log;
+
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -9,11 +11,15 @@ import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 import org.firstinspires.ftc.teamcode.Movement;
+import org.firstinspires.ftc.teamcode.SahilClass;
 import org.firstinspires.ftc.teamcode.TestingOpModes.TFODTest;
 import org.firstinspires.ftc.teamcode.watchdog.IMUWatchdog;
 import org.firstinspires.ftc.teamcode.watchdog.WatchdogManager;
+import org.opencv.android.OpenCVLoader;
 
+import virtualRobot.VuforiaLocalizerImplSubclass;
 import virtualRobot.utils.MathUtils;
 
 import static org.firstinspires.ftc.teamcode.Movement.distToEncoder;
@@ -40,6 +46,15 @@ public class BlueAuton1 extends LinearOpMode {
 //    final float targetRadius = 10+c; //inches
 //    final double targetSpeed =4; //inches/sec
 //    final double encoderSpeed = (targetSpeed/(2*Math.PI*wheelRadius)) * ticksPerRev; //encoders per sec
+
+    static {
+        if(OpenCVLoader.initDebug()) {
+            Log.d("opencv","yay it works");
+        } else {
+            Log.d("opencv","nope it doesnt work");
+        }
+    }
+
     @Override
     public void runOpMode() throws InterruptedException {
         lf = hardwareMap.dcMotor.get("leftFront");
@@ -106,7 +121,19 @@ public class BlueAuton1 extends LinearOpMode {
         mv.translateDistance(0.7,-15);
 //        mv.rotateTo(0);
 
-        int num = 1;//tfod.getGoldPos();//TODO:Fix //which mineral is the gold mineral one
+        VuforiaLocalizer.Parameters params = new VuforiaLocalizer.Parameters(hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName()));
+        params.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
+
+        params.vuforiaLicenseKey = "AdVGalv/////AAAAGYhiDIdk+UI+ivt0Y7WGvUJnm5cKX/lWesW2pH7gnK3eOLTKThLekYSO1q65ttw7X1FvNhxxhdQl3McS+mzYjO+HkaFNJlHxltsI5+b4giqNQKWhyKjzbYbNw8aWarI5YCYUFnyiPPjH39/CbBzzFk3G2RWIzNB7cy4AYhjwYRKRiL3k33YvXv0ZHRzJRkMpnytgvdv5jEQyWa20DIkriC+ZBaj8dph8/akyYfyD1/U19vowknmzxef3ncefgOZoI9yrK82T4GBWazgWvZkIz7bPy/ApGiwnkVzp44gVGsCJCUFERiPVwfFa0SBLeCrQMrQaMDy3kOIVcWTotFn4m1ridgE5ZP/lvRzEC4/vcuV0";
+
+        VuforiaLocalizerImplSubclass vuforiaInstance = new VuforiaLocalizerImplSubclass(params);
+        waitForStart();
+
+        SahilClass sahilClass = new SahilClass(vuforiaInstance, 1000); //this only loops once after 1000 millis but keep this constraint just in case
+//        while (!Thread.currentThread().isInterrupted()) {
+        int num = sahilClass.getPosition();
+        telemetry.addData("Position", num + "");
+        telemetry.update();
 //        tfod.clean();
 //        String name[] = new String[] {"LEFT", "CENTER", "RIGHT"};
 //        telemetry.addData("Mineral", num > -1 && num < 4 ? name[num] : "UNKNOWN");
@@ -115,20 +142,25 @@ public class BlueAuton1 extends LinearOpMode {
         //TODO: SWITCH TO ROTATETO
         switch (num) {
             case 0:
-                mv.rotateDegrees(0.5,60);
+                mv.rotateTo(45);
                 mv.translateDistance(0.7,-24);
-                mv.rotateDegrees(0.5, -60);
+                mv.rotateTo(-45);
+                mv.translateDistance(0.7, -24);
+                mv.rotateTo(0);
                 break;
             case 2:
-                mv.rotateDegrees(0.5,-60);
+                mv.rotateTo(-45);
                 mv.translateDistance(0.7,-24);
-                mv.rotateDegrees(0.5, 60);
+                mv.rotateTo(45);
+                mv.translateDistance(0.7, -24);
+                mv.rotateTo(0);
                 break;
             default:
             case 1:
-                mv.translateDistance(0.7,-35);
+                mv.translateDistance(0.7,-34);
                 break;
         }
+        mv.translateDistance(0.7, -17);
 
 //        mv.moveToPosition(new DcMotor[] {liftL, liftR}, new double[] {-0.8, 0.8}, new int[] {600, 600});
         liftL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
