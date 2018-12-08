29e2e17f9bd938618b1a84b8a277e89d6e557946
SahilShah518 <sahilshah379@gmail.com>
2018-12-07 22:21:20 -0500

    teleop reaper

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NonUpdateCompetitionOpModes/NewNewNewTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NonUpdateCompetitionOpModes/NewNewNewTeleOp.java
@@ -6,12 +6,25 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
-
+import org.firstinspires.ftc.teamcode.JeffBot;
 import virtualRobot.utils.MathUtils;
-
 import static com.qualcomm.robotcore.hardware.DcMotorSimple.Direction.REVERSE;
+
+/*
+Controls:
+Joystick left - forward/backward
+Joystick right - left/right
+Dpad up and down - lift
+Dpad left and right - reaper extends forward/backward
+a - reset every damn thing
+x - toggle locking lift
+y - toggle dumping mineral servo
+right/left bumper - hold to spin reaper forward/backward
+trigger - folding reaper forward/backward
+ */
 
 @TeleOp(name = "REAL TeleOp", group = "competition")
 public class NewNewNewTeleOp extends OpMode {
@@ -19,16 +32,23 @@ public class NewNewNewTeleOp extends OpMode {
     private DcMotorEx lb;
     private DcMotorEx rf;
     private DcMotorEx rb;
-    private DcMotorEx reaperL;
-    private DcMotorEx reaperR;
-    private DcMotorEx liftL;
-    private DcMotorEx liftR;
-//    private CRServo reaper;
+    private DcMotorEx reaperLeft;
+    private DcMotorEx reaperRight;
+    private DcMotorEx liftLeft;
+    private DcMotorEx liftRight;
+    private CRServo reaperSpin;
     private Servo stopper;
+    private Servo dropper;
+    private Servo reaperFoldLeft;
+    private Servo reaperFoldRight;
+    private JeffBot mv = new JeffBot();
+
+
     double power = 0.9;
     double gearing = 1;
 
     private ElapsedTime canToggleStopper;
+    private ElapsedTime canToggleDropper;
 
     @Override
     public void init() {
@@ -38,51 +58,59 @@ public class NewNewNewTeleOp extends OpMode {
         lb = (DcMotorEx)hardwareMap.dcMotor.get("leftBack");
         rf = (DcMotorEx)hardwareMap.dcMotor.get("rightFront");
         rb = (DcMotorEx)hardwareMap.dcMotor.get("rightBack");
-        liftL = (DcMotorEx)hardwareMap.dcMotor.get("liftL");
-        liftR = (DcMotorEx)hardwareMap.dcMotor.get("liftR");
-        reaperL = (DcMotorEx)hardwareMap.dcMotor.get("horizL");
-        reaperR = (DcMotorEx)hardwareMap.dcMotor.get("horizR");
-//        reaper = hardwareMap.crservo.get("reaper");
+        liftLeft = (DcMotorEx)hardwareMap.dcMotor.get("liftL");
+        liftRight = (DcMotorEx)hardwareMap.dcMotor.get("liftR");
+        reaperLeft = (DcMotorEx)hardwareMap.dcMotor.get("horizL");
+        reaperRight = (DcMotorEx)hardwareMap.dcMotor.get("horizR");
+        reaperSpin = hardwareMap.crservo.get("reaper");
         stopper = hardwareMap.servo.get("stopper");
+        dropper = hardwareMap.servo.get("dropper");
+        reaperFoldLeft = hardwareMap.servo.get("reaperFoldL");
+        reaperFoldRight = hardwareMap.servo.get("reaperFoldT");
+
 
         lf.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         lb.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         rf.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         rb.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        liftL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        liftR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        liftLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        liftRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         lf.setDirection(DcMotorSimple.Direction.FORWARD);
         lb.setDirection(DcMotorSimple.Direction.FORWARD);
-
         lf.setDirection(REVERSE);
         lb.setDirection(REVERSE);
-        liftR.setDirection(REVERSE);
+        liftRight.setDirection(REVERSE);
+        reaperFoldRight.setDirection(Servo.Direction.REVERSE);
 
         lf.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         lb.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         rf.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         rb.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        liftL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        liftR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        reaperL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        reaperR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        liftLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        liftRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        reaperLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        reaperRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         lf.setPower(0);
         lb.setPower(0);
         rf.setPower(0);
         rb.setPower(0);
-        liftL.setPower(0);
-        liftR.setPower(0);
-        reaperL.setPower(0);
-        reaperR.setPower(0);
-//        reaper.setPower(0);
+        liftLeft.setPower(0);
+        liftRight.setPower(0);
+        reaperLeft.setPower(0);
+        reaperRight.setPower(0);
+        reaperSpin.setPower(0);
         stopper.setPosition(1);
+        dropper.setPosition(0);
+        reaperFoldLeft.setPosition(0.1);
+        reaperFoldRight.setPosition(0.1);
     }
 
     @Override
     public void loop() {
 
+        // robot movement
         if (!MathUtils.equals(gamepad1.right_stick_x,0)) {
             lf.setPower(-1 * power * gearing * gamepad1.right_stick_x);
             lb.setPower(-1 * power * gearing * gamepad1.right_stick_x);
@@ -100,64 +128,93 @@ public class NewNewNewTeleOp extends OpMode {
             rb.setPower(0);
         }
 
+        // lift movement
         if(gamepad1.dpad_up) {
-            liftL.setPower(1 * gearing);
-            liftR.setPower(1 * gearing);
+            liftLeft.setPower(1 * gearing);
+            liftRight.setPower(1 * gearing);
         } else if (gamepad1.dpad_down) {
-            liftL.setPower(-1 * gearing);
-            liftR.setPower(-1 * gearing);
+            liftLeft.setPower(-1 * gearing);
+            liftRight.setPower(-1 * gearing);
         } else {
-            liftL.setPower(0);
-            liftR.setPower(0);
+            liftLeft.setPower(0);
+            liftRight.setPower(0);
         }
 
+        // reaper extension
         if (gamepad1.dpad_left) {
-            gearing = 1;
+            reaperLeft.setPower(1 * gearing);
+            reaperRight.setPower(1 * gearing);
         } else if (gamepad1.dpad_right) {
-            gearing = 0.5;
+            reaperLeft.setPower(-1 * gearing);
+            reaperRight.setPower(-1 * gearing);
+        } else {
+            reaperLeft.setPower(0);
+            reaperRight.setPower(0);
         }
 
+
+        // you spin me right round
+        if (gamepad1.left_bumper) {
+            reaperSpin.setPower(1);
+        } else if (gamepad1.right_bumper) {
+            reaperSpin.setPower(-1);
+        } else {
+            reaperSpin.setPower(0);
+        }
+
+
+        // dump truck
+        if (gamepad1.left_trigger == 1) {
+            reaperFoldLeft.setPosition(1);
+            reaperFoldLeft.setPosition(1);
+        } else if (gamepad1.right_trigger == 1) {
+            reaperFoldLeft.setPosition(0.1);
+            reaperFoldRight.setPosition(0.1);
+        }
+
+
+
+        // reset pls
         if (gamepad1.a) {
             lf.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
             lb.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
             rf.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
             rb.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-            liftL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-            liftR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            liftLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            liftRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            reaperLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            reaperRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
             lf.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
             lb.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
             rf.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
             rb.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            liftL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            liftR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            liftLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            liftRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            reaperLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            reaperRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         }
 
 
-
-        if (!MathUtils.equals(gamepad2.right_stick_x,0)) {
-//            reaper.setPower(Math.signum(gamepad2.right_stick_x));
-        } else {
-//            reaper.setPower(0);
+        // where we droppin boys?
+        if (gamepad1.y) {
+            int initialPosition = 0;
+            int finalPosition = 1;
+            if (dropper.getPosition() == initialPosition) {
+                dropper.setPosition(finalPosition);
+            } else {
+                dropper.setPosition(initialPosition);
+            }
         }
 
-        if(gamepad2.dpad_up) {
-            reaperL.setPower(1 * gearing);
-            reaperR.setPower(1 * gearing);
-        } else if (gamepad2.dpad_down) {
-            reaperL.setPower(-1 * gearing);
-            reaperR.setPower(-1 * gearing);
-        } else {
-            reaperL.setPower(0);
-            reaperR.setPower(0);
+
+        // toggle dropper
+        if (gamepad1.y && canToggleDropper.milliseconds() > 500) {
+            dropper.setPosition(1 - dropper.getPosition());
+            canToggleDropper.reset();
         }
 
-        if (gamepad2.a) {
-            reaperL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-            reaperR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-            reaperL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            reaperR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        }
 
+        // toggle locking lift
         if (gamepad1.x && canToggleStopper.milliseconds() > 500) {
             stopper.setPosition(1 - stopper.getPosition());
             canToggleStopper.reset();
@@ -165,15 +222,19 @@ public class NewNewNewTeleOp extends OpMode {
 
 
 
-        telemetry.addData("LF", lf.getCurrentPosition() + "");
-        telemetry.addData("LB", lb.getCurrentPosition() + "");
-        telemetry.addData("RF", rf.getCurrentPosition() + "");
-        telemetry.addData("RB", rb.getCurrentPosition() + "");
-        telemetry.addData("LIFTL", liftL.getCurrentPosition() + "");
-        telemetry.addData("LIFTR", liftR.getCurrentPosition() + "");
-        telemetry.addData("REAPERL", reaperL.getCurrentPosition() + "");
-        telemetry.addData("REAPERR", reaperR.getCurrentPosition() + "");
-//        telemetry.addData("REAPER", reaper.getPower() + "");
+        telemetry.addData("lf", lf.getCurrentPosition() + "");
+        telemetry.addData("lb", lb.getCurrentPosition() + "");
+        telemetry.addData("rf", rf.getCurrentPosition() + "");
+        telemetry.addData("rb", rb.getCurrentPosition() + "");
+        telemetry.addData("lift l", liftLeft.getCurrentPosition() + "");
+        telemetry.addData("lift r", liftRight.getCurrentPosition() + "");
+        telemetry.addData("reaper extension l", reaperLeft.getCurrentPosition() + "");
+        telemetry.addData("reaper extension r", reaperRight.getCurrentPosition() + "");
+        telemetry.addData("reaper power", reaperSpin.getPower() + "");
+        telemetry.addData("stopper", stopper.getPosition() + "");
+        telemetry.addData("dropper", dropper.getPosition() + "");
+        telemetry.addData("reaper fold l", reaperFoldLeft.getPosition() + "");
+        telemetry.addData("reaper fold r", reaperFoldRight.getPosition() + "");
     }
 
     @Override
