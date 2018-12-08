faebd0ac6cc51d183f16743c22bf758dda086ecc
David Shustin <david.shustin@gmail.com>
2018-12-08 13:33:16 -0500

    Update TomasClass

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestingOpModes/TomasClass.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestingOpModes/TomasClass.java
@@ -3,19 +3,28 @@ package org.firstinspires.ftc.teamcode.TestingOpModes;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 
 @TeleOp(name = "TomasClass", group = "testing")
 public class TomasClass extends OpMode {
     private DcMotor horizR;
+    private DcMotor horizL;
 
     @Override
     public void init() {
         horizR = hardwareMap.dcMotor.get("horizR");
+        horizL = hardwareMap.dcMotor.get("horizL");
+//        horizL.setDirection(DcMotorSimple.Direction.REVERSE);
+        horizR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        horizL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
 
     @Override
     public void loop() {
         horizR.setPower(gamepad1.left_stick_y * 0.5);
+        horizL.setPower(gamepad1.left_stick_y * 0.5);
+        telemetry.addData("Left", horizL.getCurrentPosition());
+        telemetry.addData("Right", horizR.getCurrentPosition());
     }
 }
